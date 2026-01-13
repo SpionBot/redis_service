b3882ed5f7542c4d1d756aa5a521bf00e8545970
Руслан <stla7824@gmail.com>
2026-01-13 20:59:03 +0700

    поменял промт

---
+++ FFF.py
@@ -0,0 +1,17 @@
+import requests
+
+url = "https://redis-service-1.onrender.com/check_connection"
+
+payload = {
+    "password": "669ad350340678994430b9605aec8c341171844f64d56a452bc2f806b3355cec10afd1c602e79c717e974b35e44ed7b5e25f847f1bff29f49cf66830136666af",
+    "game": "dota2"
+}
+
+headers = {
+    "accept": "application/json",
+    "Content-Type": "application/json"
+}
+response = requests.post(url, json=payload, headers=headers)
+
+print(response.status_code)
+print(response.json()["result"])
